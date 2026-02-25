04959019619f5b5f4e110d3247f3b3a0330d0d93
Nicolas Munnich <munnich@lipn.univ-paris13.fr>
2026-02-25 16:01:02 +0100

    qwd

--- test/west-commands/west.py
+++ test/west-commands/west.py
@@ -11,5 +11,13 @@ class West(WestCommand):
             description="Print hello world to console",
         )
 
+    def do_add_parser(self, parser_adder):
+        parser = parser_adder.add_parser(
+            self.name,
+            help=self.help,
+            description=self.description,
+        )
+        return parser
+
     def do_run(self, args, unknown_args):
         print("hello world")
